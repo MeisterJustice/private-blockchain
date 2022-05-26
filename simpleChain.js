f76f2b07ef3188d5699202fc4ca06817ec35c5a2
Justice Eziefule <thechiefje@gmail.com>
2022-05-26 23:06:04 +0400

    add simpleChain classes

---
+++ simpleChain.js
@@ -0,0 +1,27 @@
+/* ================ Block Class =================
+    Class with a constructor for block
+    ===========================================*/
+
+class Block {
+    constructor(data){
+        this.hash = "",
+        this.height = 0,
+        this.body = data,
+        this.time = 0,
+        this.previousblockhash = ""
+    }
+}
+
+/* ================ Block Class =================
+    Class with a constructor for new blockchain
+    ===========================================*/
+
+class Blockchain {
+    constructor(){
+        this.chain = []
+    }
+
+    addBlock (newBlock) {
+        this.chain.push(newBlock);
+    }
+}
