2f30888bde419b5af60c5d5bcb56954cb2cd493a
Justice Eziefule <thechiefje@gmail.com>
2022-05-26 23:35:58 +0400

    add extra functions

--- simpleChain.js
+++ simpleChain.js
@@ -14,14 +14,50 @@ class Block {
     }
 }
 
-/* ================ Block Class =================
-    Class with a constructor for new blockchain
-    ===========================================*/
+/* ===== SHA256 with Crypto-js ===================================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js      |
+|  =============================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+
+/* ===== Block Class ===================================
+|  Class with a constructor for block data model       |
+|  ====================================================*/
+
+class Block {
+  constructor(data){
+    this.height = '';
+    this.timeStamp = '';
+    this.data = data;
+    this.previousHash = '0x';
+    this.hash = '';
+  }
+}
+
+/* ===== Blockchain ===================================
+|  Class with a constructor for blockchain data model  |
+|  with functions to support:                          |
+|     - createGenesisBlock()                           |
+|     - getLatestBlock()                               |
+|     - addBlock()                                     |
+|     - getBlock()                                     |
+|     - validateBlock()                                |
+|     - validateChain()                                |
+|  ====================================================*/
+
 
 class Blockchain {
     constructor(){
         this.chain = [],
-        this.addBlock(new Block("First block in the chain - Genesis block"));
+        this.addBlock(this.createGenesisBlock());
+    }
+
+    createGenesisBlock () {
+        return new Block("First block in the chain - Genesis block")
+    }
+
+    getLatestBlock () {
+        return this.chain[this.chain.length - 1]
     }
 
     addBlock (newBlock) {
