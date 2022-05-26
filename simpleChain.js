6036befcc8b4cef84f697fd7b06eb097d0d64b7a
Justice Eziefule <thechiefje@gmail.com>
2022-05-26 23:31:09 +0400

    configure newBlock object

--- simpleChain.js
+++ simpleChain.js
@@ -25,6 +25,9 @@ class Blockchain {
     }
 
     addBlock (newBlock) {
+        newBlock.height = this.chain.length;
+        newBlock.time = new Date().getTime().toString().slice(0, -3);
+
         if(this.chain.length > 0){
             newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash
         }
